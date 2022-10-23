fc51fe55dddc3514f2a1feacc0576810d111226b
Jhonatan <jonathandiazp@gmail.com>
2022-10-23 17:51:22 -0400

    getsinglethought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -8,4 +8,15 @@ module.exports = {
       .catch((err) => res.status(500).json(err));
   },
 
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .select("-__v")
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No Thought find with this ID!" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  
 };
