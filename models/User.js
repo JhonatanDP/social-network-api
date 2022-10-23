2894ada9c29aa283eb361800a1aacb5b9a667b3d
Jhonatan <jonathandiazp@gmail.com>
2022-10-23 19:42:19 -0400

    fixing API routes

--- models/User.js
+++ models/User.js
@@ -14,7 +14,7 @@ const userSchema = new Schema(
       unique: true,
       match: [
         /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-        "Please fill a valid email address",
+        "Please enter a valid email",
       ],
     },
     thoughts: [
