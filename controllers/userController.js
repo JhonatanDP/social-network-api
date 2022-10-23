45ff769ae60ee5eb1ffdf4d30c1179a2e046b8f1
Jhonatan <jonathandiazp@gmail.com>
2022-10-23 17:34:00 -0400

    deletefriend

--- controllers/userController.js
+++ controllers/userController.js
@@ -63,12 +63,25 @@ module.exports = {
     )
       .then((user) =>
         !user
-          ? res.status(404).json({ message: "No User find with this ID!" })
+          ? res.status(404).json({ message: "No User found" })
           : res.json(user)
       )
       .catch((err) => res.status(500).json(err));
   },
 
-  
+  deleteFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: req.params.friendId } },
+      { new: true }
+    )
+      .then(
+        (user) =>
+          !user
+            ? res.status(404).json({ message: "No User found" })
+            : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 
 };
