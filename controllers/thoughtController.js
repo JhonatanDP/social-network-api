ea2d4d0bbaf86b2ac41ba84a60f803d33873fc03
Jhonatan <jonathandiazp@gmail.com>
2022-10-23 18:24:16 -0400

    deletethought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -44,8 +44,27 @@ module.exports = {
     )
       .then((user) =>
         !user
-          ? res.status(404).json({ message: "No thought find with this ID!" })
+          ? res.status(404).json({ message: "No thought found" })
           : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  deleteThought(req, res) {
+    Thought.findOneAndDelete({ _id: req.params.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought found" })
+          : User.findOneAndUpdate(
+              { thoughts: req.params.thoughtId },
+              { $pull: { thoughts: req.params.thoughtId } },
+              { new: true }
+            )
+      )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'User no found but Thought deleted'})
+          : res.json({ message: 'Thought deleted' })
       )
       .catch((err) => res.status(500).json(err));
   },
