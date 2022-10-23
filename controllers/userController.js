199909deb8bfa53bac144af4a48757797800a01c
Jhonatan <jonathandiazp@gmail.com>
2022-10-23 16:58:46 -0400

    updateuser

--- controllers/userController.js
+++ controllers/userController.js
@@ -30,4 +30,21 @@ module.exports = {
       });
   },
 
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No User find with this ID!" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  
+
+
 };
